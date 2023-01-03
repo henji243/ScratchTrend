f10c675c0d89b4d5ad02dfabe178bf7a2dbc73e0
henji243 <greentai3025@gmail.com>
2023-01-03 10:49:41 +0900

    テストコードの修正

--- test/test_run.py
+++ test/test_run.py
@@ -1,12 +1,8 @@
-import sys
-from os.path import dirname, abspath
-sys.path.append(dirname(dirname(abspath(__file__))))
-
 import scratchtrend as sct
 from scratchtrend.select import Lang, Sort
 
 
 data = sct.connect(Lang.ENGLISH, Sort.POPULAR)
-res = data.get_by_num(1, 100)
+res = data.get_by_rank(1, 100)
 
 print(res)
